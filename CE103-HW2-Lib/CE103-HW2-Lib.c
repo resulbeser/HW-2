8f1ad31336b7637d3d0bd410b0ffa4cd0b8fc0d4
Ugur Coruh <ugur.coruh.tr@gmail.com>
2021-11-03 03:53:56 +0300

    initial commit

---
+++ CE103-HW2-Lib/CE103-HW2-Lib.c
@@ -0,0 +1,80 @@
+/****************************************************************************
+ * Copyleft (L) 2021 CENG - All Rights Not Reserved
+ * You may use, distribute and modify this code.
+ ****************************************************************************/
+
+ /**
+  * @file CE103-HW2-Lib.c
+  * @author Uður CORUH
+  * @date 03 October 2021
+  *
+  * @brief <b> HW-2 Functions </b>
+  *
+  * HW-2 Sample Lib Functions
+  *
+  * @see http://bilgisayar.mmf.erdogan.edu.tr/en/
+  *
+  */
+
+#include "CE103-HW2-Lib.h"
+
+  /** \copydoc fnCE103HW2Lib */
+int fnCE103HW2Lib(unsigned char* fia, int fib, char* fic)
+{
+	//Empty Body...
+	return -1;
+}
+
+/** \copydoc ce103_fibonacciNumber */
+int ce103_fibonacciNumber(int fiIndex)
+{
+	//TODO:Start from Here...
+	return -1;
+}
+
+/** \copydoc ce103_strrev */
+char* ce103_strrev(char* fiStr)
+{
+	//TODO:Start from Here...
+	return 0;
+}
+
+/** \copydoc ce103_strlen */
+int ce103_strlen(const char* fiStr)
+{
+	//TODO:Start from Here...
+	return -1;
+}
+
+/** \copydoc ce103_strcat */
+char* ce103_strcat(char* fiDest, char* fiSrc)
+{
+	//TODO:Start from Here...
+	return 0;
+}
+
+/** \copydoc ce103_strcmp */
+int ce103_strcmp(const char* fiLhs, const char* fiRhs)
+{
+	//TODO:Start from Here...
+	return -2;
+}
+
+/** \copydoc ce103_strcpy */
+char* ce103_strcpy(char* foDestination, const char* fiSource)
+{
+	//TODO:Start from Here...
+	return 0;
+}
+
+/** \copydoc ce103_hex2bin */
+void ce103_hex2bin(char* fiHex, int fiHexLen, unsigned char* foBin)
+{
+	//TODO:Start from Here...
+}
+
+/** \copydoc ce103_bin2hex */
+void ce103_bin2hex(unsigned char* fiBin, int fiBinLen, char* foHex)
+{
+	//TODO:Start from Here...
+}
---
+++ CE103-HW2/CE103-HW2-Test-App.c
@@ -0,0 +1,11 @@
+#pragma comment(lib, "..\\Debug\\CE103-HW2-Lib.lib")
+#include "..\CE103-HW2-Lib\CE103-HW2-Lib.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("CE103 Algorithms and Programming I HW-2\n");
+
+	getch();
+
+}
